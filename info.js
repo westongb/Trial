ca3062827cb13eebaac3bc743601dfb1a1471195
westongb <westongb@outlook.com>
2019-05-19 18:30:57 -0600

    git commit

---
+++ info.js
@@ -0,0 +1,12 @@
+
+function myFunction(){
+    document.getElementById(Input1).submit();
+    document.getElementById(Input2).submit();
+    document.getElementById(Input3).submit();
+    }
+
+    console.log("Input1");
+
+    console.log("Input2");
+
+    console.log("Input3");
